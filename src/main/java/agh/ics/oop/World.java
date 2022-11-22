1f123fa56bff4c4db93c3a61c05fc5b7ff341185
tudde <tuddeum@gmail.com>
2022-11-22 07:15:00 +0100

    Lab5

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -8,8 +8,7 @@ public class World {
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        System.out.println(map.toString());
-
+        
 
     }
 
